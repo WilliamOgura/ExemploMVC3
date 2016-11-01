7ddb576459edd81f4b49a99701ea98b960b04d53
Willian Novaes <r2w2@live.com>
2016-11-01 18:16:36 -0200

    implementação do metodo cadastrar

---
+++ Fiap.Exemplo02.MVC.Web/Controllers/AlunoController.cs
@@ -0,0 +1,28 @@
+﻿using Fiap.Exemplo02.MVC.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exemplo02.MVC.Web.Controllers
+{
+    public class AlunoController : Controller
+    {
+        private PortalContext _context = new PortalContext();
+        // GET: Aluno
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastro(Aluno aluno)
+        {
+            _context.Aluno.Add(aluno);
+            _context.SaveChanges();
+            TempData["msg"] = "Aluno Cadastrado";
+            return RedirectToAction("Cadastro");
+        }
+    }
+}
---
+++ Fiap.Exemplo02.MVC.Web/Controllers/EcommerceContext.cs
@@ -0,0 +1,10 @@
+﻿using Fiap.Exemplo02.MVC.Web.Models;
+using System.Data.Entity;
+
+namespace Fiap.Exemplo02.MVC.Web.Controllers
+{
+    public class EcommerceContext : PortalContext
+    {
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
